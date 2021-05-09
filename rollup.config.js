516c0e83575f9b07d73f38b2a281fabc2569c48e
ScottAgirs <scott@ijs.to>
2021-05-09 17:56:23 -0400

    feat(Rollup): (WIP) Init
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import { babel } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+// Will
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+// This will check all ESLinting while compiling.
+// Useful because will notify if anything should be fixed.
+// import { eslint } from 'rollup-plugin-eslint';
+import json from '@rollup/plugin-json';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    dir: 'dist',
+    format: 'cjs',
+  },
+  plugins: [
+    // eslint(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    }),
+    // CommonJS Must be after babel.
+    // https://github.com/rollup/plugins/tree/master/packages/babel#using-with-rollupplugin-commonjs
+    commonjs(),
+    nodeResolve(),
+    json(),
+  ],
+};
